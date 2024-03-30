864e3c4b624fbbd6bce2fa14c5bbfcbb0edfd893
markliquid1 <139247086+markliquid1@users.noreply.github.com>
2024-03-30 01:25:27 +0100

    Update UserInterface.ino
    First time working with communication in 2 directions
    Sensor data spoofed

--- UserInterface.ino
+++ UserInterface.ino
@@ -1,12 +1,20 @@
 /*********
+  X Engineering March 2024
+  This is the beginning of a user interface to configure and display sensor readings from the Regulator.
+  The display readings are done with ServerSentEvents
+  The configuration parameters are uploaded with HTTP_GET requests.  The parameters are also stored permanently in SPIFFS so they remain after a power cycle.
+  For now, there are no real hardware sensors, so the sensor readings are spoofed using variable N which increments continuously.   
+  It should not be difficult from here to make the sensor data real. 
+
+*************************
+  Code skeleton came from the below source:
   Rui Santos
   Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-input-data-html-form/
-  
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files.
-  
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
+  **** Beware the write limitation for EEProm*** (not an issue here)
 *********/
 
 #include <Arduino.h>
@@ -21,23 +29,35 @@ AsyncWebServer server(80);
 const char* ssid = "MN2G";
 const char* password = "5FENYC8PDW";
 
+// Create an Event Source on /events
+AsyncEventSource events("/events");
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 500;
+
+float AlternatorTemperature;
+float FieldDutyCycle;
+float BatteryVoltage;
+int N = 1;
+
 const char* TLimit = "TemperatureLimitF";
 const char* FieldCurrentSetp = "FieldCurrentSetpointAmps";
 const char* FullChargeV = "FullChargeVoltage";
 
-// HTML web page to handle 3 input fields (inputString, FieldCurrentSetpointAmps, FullChargeVoltage)
+// HTML web page
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html><head>
-  <title>ESP Input Form</title>
+  <title>Alt Regulator by X Engineering</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script>
     function submitMessage() {
       alert("Saved value to ESP SPIFFS");
-      setTimeout(function(){ document.location.reload(false); }, 500);   
+      setTimeout(function(){ document.location.reload(false); }, 1000);   
     }
   </script></head><body>
   <form action="/get" target="hidden-form">
-    TemperatureLimitF (current value %TemperatureLimitF%): <input type="text" name="TemperatureLimitF">
+    TemperatureLimitF (current value %TemperatureLimitF%): <input type="number " name="TemperatureLimitF">
     <input type="submit" value="Submit" onclick="submitMessage()">
   </form><br>
   <form action="/get" target="hidden-form">
@@ -49,37 +69,110 @@ const char index_html[] PROGMEM = R"rawliteral(
     <input type="submit" value="Submit" onclick="submitMessage()">
   </form>
   <iframe style="display:none" name="hidden-form"></iframe>
-</body></html>)rawliteral";
+
+
+
+ <div class="content">
+    <div class="cards">
+      <div class="card">
+        <p><i class="fas fa-thermometer-half" style="color:#059e8a;"></i> AlternatorTemperature</p><p><span class="reading"><span id="AltTempID">%AlternatorTemperature%</span> &deg;C</span></p>
+      </div>
+      <div class="card">
+        <p><i class="fas fa-angle-double-down" style="color:#00add6;"></i> FieldDutyCycle</p><p><span class="reading"><span id="FieldDutyCycleID">%FieldDutyCycle%</span> &percnt;</span></p>
+      </div>
+      <div class="card">
+        <p><i class="fas fa-battery-three-quarters" style="color:#e1e437;"></i> BatteryVoltage</p><p><span class="reading"><span id="BatteryVoltageID">%BatteryVoltage%</span> V</span></p>
+      </div>
+    </div>
+  </div>
+<script>
+if (!!window.EventSource) {
+ var source = new EventSource('/events');
+
+ source.addEventListener('open', function(e) {
+  console.log("Events Connected");
+ }, false);
+ source.addEventListener('error', function(e) {
+  if (e.target.readyState != EventSource.OPEN) {
+    console.log("Events Disconnected");
+  }
+ }, false);
+
+ source.addEventListener('message', function(e) {
+  console.log("message", e.data);
+ }, false);
+
+ source.addEventListener('AlternatorTemperature', function(e) {
+  console.log("AlternatorTemperature", e.data);
+  document.getElementById("AltTempID").innerHTML = e.data;
+ }, false);
+
+ source.addEventListener('FieldDutyCycle', function(e) {
+  console.log("FieldDutyCycle", e.data);
+  document.getElementById("FieldDutyCycleID").innerHTML = e.data;
+ }, false);
+
+ source.addEventListener('BatteryVoltage', function(e) {
+  console.log("BatteryVoltage", e.data);
+  document.getElementById("BatteryVoltageID").innerHTML = e.data;
+ }, false);
+}
+</script>
+</body>
+</html>)rawliteral";
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("RRSI (lower absolute value is better!): ");
+  Serial.println(WiFi.RSSI());
+}
+
+void getSensorReadings() {
+  AlternatorTemperature = 1;
+  FieldDutyCycle = 2.3;
+  BatteryVoltage = 3.42;
+}
 
 void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
 }
 
-String readFile(fs::FS &fs, const char * path){
+String readFile(fs::FS &fs, const char * path) {
   Serial.printf("Reading file: %s\r\n", path);
   File file = fs.open(path, "r");
-  if(!file || file.isDirectory()){
+  if (!file || file.isDirectory()) {
     Serial.println("- empty file or failed to open file");
     return String();
   }
   Serial.println("- read from file:");
   String fileContent;
-  while(file.available()){
-    fileContent+=String((char)file.read());
+  while (file.available()) {
+    fileContent += String((char)file.read());
   }
   file.close();
   Serial.println(fileContent);
   return fileContent;
 }
 
-void writeFile(fs::FS &fs, const char * path, const char * message){
+void writeFile(fs::FS &fs, const char * path, const char * message) {
   Serial.printf("Writing file: %s\r\n", path);
   File file = fs.open(path, "w");
-  if(!file){
+  if (!file) {
     Serial.println("- failed to open file for writing");
     return;
   }
-  if(file.print(message)){
+  if (file.print(message)) {
     Serial.println("- file written");
   } else {
     Serial.println("- write failed");
@@ -88,15 +181,26 @@ void writeFile(fs::FS &fs, const char * path, const char * message){
 }
 
 // Replaces placeholder with stored values
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "TemperatureLimitF"){
+String processor(const String& var) {
+  getSensorReadings();
+
+  if (var == "AlternatorTemperature") {
+    return String(AlternatorTemperature);
+  }
+  else if (var == "FieldDutyCycle") {
+    return String(FieldDutyCycle);
+  }
+  else if (var == "BatteryVoltage") {
+    return String(BatteryVoltage);
+  }
+
+  else if (var == "TemperatureLimitF") {
     return readFile(SPIFFS, "/TemperatureLimitF.txt");
   }
-  else if(var == "FieldCurrentSetpointAmps"){
+  else if (var == "FieldCurrentSetpointAmps") {
     return readFile(SPIFFS, "/FieldCurrentSetpointAmps.txt");
   }
-  else if(var == "FullChargeVoltage"){
+  else if (var == "FullChargeVoltage") {
     return readFile(SPIFFS, "/FullChargeVoltage.txt");
   }
   return String();
@@ -104,38 +208,19 @@ String processor(const String& var){
 
 void setup() {
   Serial.begin(115200);
+  initWiFi();
   // Initialize SPIFFS
-  #ifdef ESP32
-    if(!SPIFFS.begin(true)){
-      Serial.println("An Error has occurred while mounting SPIFFS");
-      return;
-    }
-  #else
-    if(!SPIFFS.begin()){
-      Serial.println("An Error has occurred while mounting SPIFFS");
-      return;
-    }
-  #endif
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("WiFi Failed!");
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
-  Serial.println();
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("RRSI (lower absolute value is better!): ");
-  Serial.println(WiFi.RSSI());
-
   // Send web page with input fields to client
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send_P(200, "text/html", index_html, processor);
   });
 
   // Send a GET request to <ESP_IP>/get?inputString=<inputMessage>
-  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest * request) {
     String inputMessage;
     // GET inputString value on <ESP_IP>/get?inputString=<inputMessage>
     if (request->hasParam(TLimit)) {
@@ -159,21 +244,42 @@ void setup() {
     request->send(200, "text/text", inputMessage);
   });
   server.onNotFound(notFound);
+
+  // Handle Web Server Events
+  events.onConnect([](AsyncEventSourceClient * client) {
+    if (client->lastId()) {
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+
   server.begin();
 }
 
 void loop() {
-  // To access your stored values 
-  String yourTemperatureLimitF = readFile(SPIFFS, "/TemperatureLimitF.txt");
-  Serial.print("*** Your inputString: ");
-  Serial.println(yourTemperatureLimitF);
-  
+  // To access your stored values
+  int yourTemperatureLimitF = readFile(SPIFFS, "/TemperatureLimitF.txt").toInt();
   int yourFieldCurrentSetpointAmps = readFile(SPIFFS, "/FieldCurrentSetpointAmps.txt").toInt();
-  Serial.print("*** Your FieldCurrentSetpointAmps: ");
-  Serial.println(yourFieldCurrentSetpointAmps);
-  
   float yourFullChargeVoltage = readFile(SPIFFS, "/FullChargeVoltage.txt").toFloat();
-  Serial.print("*** Your FullChargeVoltage: ");
-  Serial.println(yourFullChargeVoltage);
-  delay(5000);
+  if ((millis() - lastTime) > timerDelay) {
+    getSensorReadings();
+    AlternatorTemperature = AlternatorTemperature + N;
+    N = N + yourTemperatureLimitF;
+    FieldDutyCycle = FieldDutyCycle + 0.1 * N;
+    BatteryVoltage = BatteryVoltage + N * .001;
+    // Send Events to the Web Client with the Sensor Readings
+    events.send("ping", NULL, millis());
+    events.send(String(AlternatorTemperature).c_str(), "AlternatorTemperature", millis());
+    events.send(String(FieldDutyCycle).c_str(), "FieldDutyCycle", millis());
+    events.send(String(BatteryVoltage).c_str(), "BatteryVoltage", millis());
+
+    lastTime = millis();
+
+
+    delay(1000);
+  }
+
 }
