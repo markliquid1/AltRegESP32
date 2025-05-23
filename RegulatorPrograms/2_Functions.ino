1aa41d3c116b1b2ceae39b74930dfeb7e3a7b16e
markliquid1 <139247086+markliquid1@users.noreply.github.com>
2025-05-23 11:43:45 -0400

    broken

--- RegulatorPrograms/2_Functions.ino
+++ RegulatorPrograms/2_Functions.ino
@@ -1,6 +1,6 @@
 // X Engineering Alternator Regulator
 //     Copyright (C) 2025  Mark Nickerson
-
+  
 //     This program is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
 //     the Free Software Foundation, either version 3 of the License, or
